2c8b2612f3b72cda13c25517b0c957bd2e504f1f
jsonOdison <jasonkhongwir1738@gmail.com>
2022-03-22 05:53:58 -0700

    css

--- src/index.js
+++ src/index.js
@@ -2,42 +2,14 @@
 import ReactDom from  'react-dom'
 import React from 'react'
 
-const name = prompt("Enter the name")
-const age = prompt("Enter the age")
-
-const currDate=new Date()
-
-const dt=currDate.getDate()
-const mt=currDate.getMonth()
-const yr=currDate.getFullYear()
-
-const date=dt+"/"+mt+"/"+yr
-
-let result=""
-
-if (age<18){
-  result="Not eligible to Drive"
-}else{
-  result= name+"  eligible to Drive"
+const heading={
+  color:"red",
+  fontFamily:"arial",
+  backgroundColor:"yellow"
 }
-
 ReactDom.render(
   <>
-  <h1>DRIVER'S LICENSE</h1>
-  <table border="1px" cellSpacing={1}>
-    <tr>
-      <th>Date</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Results</th>
-    </tr>
-    <tr>
-      <td>{date}</td>
-      <td>{name}</td>
-      <td>{age}</td>
-      <td>{result}</td>
-    </tr>
-  </table>
+    <h1 style={heading}>Welcome to css world in react </h1>
   </>,
   document.getElementById('root')
 )

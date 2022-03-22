140a1cfabba8932da8f5c418c0e820e628419006
jsonOdison <jasonkhongwir1738@gmail.com>
2022-03-22 06:39:27 -0700

    css styled for task

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import React from 'react'
 import './index.css'
 ReactDom.render(
   <>
-    <h1>Welcome to css world in react </h1>
-    <h2>importing heading </h2>
-    <p className='para'>
-      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-      
-    </p>
+    <h1 >Welcome to css world in react </h1>
+    <h2>We offer following</h2>
+    <ul>
+      <li>ReactJs</li>
+      <li>angular</li>
+      <li>django</li>
+      <li>mangoDb</li>
+    </ul>
   </>,
   document.getElementById('root')
 

e82a3263b68f0d7f3aecabd0d05ba6d883f927f0
jsonOdison <jasonkhongwir1738@gmail.com>
2022-03-23 05:57:15 -0700

    css style if else

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 
 import ReactDom from  'react-dom'
 import React from 'react'
-
 import './index.css'
+
+let curDate=new Date()
+curDate=curDate.getHours()
+let greetings=""
+const cssStyle={}
+if(curDate>=1 && curDate<12){
+  greetings='good morning'
+  cssStyle.color='white'
+}
+else if(curDate>=12 && curDate<=16){
+  greetings="good afternon "
+  cssStyle.color='red'
+}
+else{
+  greetings="good evening "
+  cssStyle.color='orange'
+}
 ReactDom.render(
   <>
-    <h1 >Welcome to css world in react </h1>
-    <h2>We offer following</h2>
-    <ul>
-      <li>ReactJs</li>
-      <li>angular</li>
-      <li>django</li>
-      <li>mangoDb</li>
-    </ul>
+   <h1>welcome to css world in react </h1>
+   <h2 style={cssStyle}>{greetings}</h2>
   </>,
   document.getElementById('root')
 

cf59aa33d822a0d160c44d406b4be25cfcd5765d
jsonOdison <jasonkhongwir1738@gmail.com>
2022-03-29 06:16:44 -0700

    components

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 
 import ReactDom from  'react-dom'
 import React from 'react'
-import './index.css'
-
-let curDate=new Date()
-curDate=curDate.getHours()
-let greetings=""
-const cssStyle={}
-if(curDate>=1 && curDate<12){
-  greetings='good morning'
-  cssStyle.color='white'
-}
-else if(curDate>=12 && curDate<=16){
-  greetings="good afternon "
-  cssStyle.color='red'
-}
-else{
-  greetings="good evening "
-  cssStyle.color='orange'
-}
+import App from './App'
 ReactDom.render(
   <>
-   <h1>welcome to css world in react </h1>
-   <h2 style={cssStyle}>{greetings}</h2>
+  <App />
   </>,
   document.getElementById('root')
 

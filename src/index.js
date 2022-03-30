d50b9f8e1cf9506fe632127d1bf993c20497de0d
jsonOdison <jasonkhongwir1738@gmail.com>
2022-03-30 05:45:28 -0700

    components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 
 import ReactDom from  'react-dom'
 import React from 'react'
-import App from './App'
-ReactDom.render(
-  <>
-  <App />
+import course from './App'
+ReactDom.render(<>
+  <h2>Calling components named values</h2>
+  <ul>
+    <li>oracle</li>
+    <li>Java</li>
+    <li>{course}</li>
+  </ul>
   </>,
   document.getElementById('root')
 
